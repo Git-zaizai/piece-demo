6cdcbb391a6202015d65461a01cb926c80d611d2
1186977649@qq.com <1186977649@qq.com>
2023-08-20 17:33:11 +0800

    update:novel新站点，加json表格

--- src/router/admin/modules/db.ts
+++ src/router/admin/modules/db.ts
@@ -1,45 +1,54 @@
-import { AppRouteRecordRaw } from '../../types'
-import { AdminLayout } from '@/router/constant'
-import { DatabasePerson24Regular } from '@vicons/fluent'
-import { DatabaseExport } from '@vicons/tabler'
-import { renderIcon } from '@/utils'
+import { AppRouteRecordRaw } from '../../types';
+import { AdminLayout } from '@/router/constant';
+import { DatabasePerson24Regular } from '@vicons/fluent';
+import { DatabaseExport } from '@vicons/tabler';
+import { renderIcon } from '@/utils';
 
 const db: AppRouteRecordRaw = {
-    path: '/db',
-    name: 'db',
-    component: AdminLayout,
-    meta: {
-        title: '数据库',
-        icon: renderIcon(DatabasePerson24Regular)
+  path: '/db',
+  name: 'db',
+  component: AdminLayout,
+  meta: {
+    title: '数据库',
+    icon: renderIcon(DatabasePerson24Regular),
+  },
+  children: [
+    {
+      path: 'db-config',
+      name: 'db-config',
+      component: () => import('@/view/admin/db/index.vue'),
+      meta: {
+        title: '数据库导出导入',
+        icon: renderIcon(DatabaseExport),
+      },
     },
-    children: [
-        {
-            path: 'db-config',
-            name: 'db-config',
-            component: () => import('@/view/admin/db/index.vue'),
-            meta: {
-                title: '数据库导出导入',
-                icon: renderIcon(DatabaseExport)
-            }
-        },
-        {
-            path: 'db-mysql',
-            name: 'db-mysql',
-            component: () => import('@/view/admin/db/mysql.vue'),
-            meta: {
-                title: 'MySQL',
-                icon: renderIcon(DatabasePerson24Regular)
-            },
-        },
-        {
-            path: 'db-mongodb',
-            name: 'db-mongodb',
-            component: () => import('@/view/admin/db/mongodb.vue'),
-            meta: {
-                title: 'mongodb',
-                icon: renderIcon(DatabasePerson24Regular)
-            },
-        },
-    ],
-}
-export default db
+    {
+      path: 'db-mysql',
+      name: 'db-mysql',
+      component: () => import('@/view/admin/db/mysql.vue'),
+      meta: {
+        title: 'MySQL',
+        icon: renderIcon(DatabasePerson24Regular),
+      },
+    },
+    {
+      path: 'db-mongodb',
+      name: 'db-mongodb',
+      component: () => import('@/view/admin/db/mongodb.vue'),
+      meta: {
+        title: 'mongodb',
+        icon: renderIcon(DatabasePerson24Regular),
+      },
+    },
+    {
+      path: 'db-json',
+      name: 'db-json',
+      component: () => import('@/view/admin/db/jsondb.vue'),
+      meta: {
+        title: 'JSON文件',
+        icon: renderIcon(DatabasePerson24Regular),
+      },
+    },
+  ],
+};
+export default db;
---
+++ src/view/admin/db/jsondb.vue
@@ -0,0 +1,7 @@
+<template><zai-table></zai-table></template>
+
+<script setup lang="ts" name="jsondb">
+import { ZaiTable } from '@/components/table';
+</script>
+
+<style scoped lang="scss"></style>
