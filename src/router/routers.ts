e38fa8b73c3d99b6ad23b32650084101a9cbc080
wqf <1186977649@qq.com>
2024-02-05 01:14:05 +0800

    初步完成md转vue

--- src/router/routers.ts
+++ src/router/routers.ts
@@ -17,6 +17,18 @@ const Root: AppRouteRecordRaw = {
 }
 
 /**
+ * @param 主路由
+ */
+const test: AppRouteRecordRaw = {
+    path: '/test',
+    name: 'test',
+    component: () => import('@/view/Test/test/test.vue'),
+    meta: {
+        title: 'test'
+    },
+}
+
+/**
  * @function 自动注册路由
  */
 const routeModules = import.meta.glob('./home/*.ts', { eager: true })
@@ -27,15 +39,15 @@ for (const key in routeModules) {
     const mod = routeModules[key] as { default: AppRouteRecordRaw }
     if (Array.isArray(mod.default)) {
         routeModuleList.push(...mod.default)
-        mod.default.forEach(modv=> hemoRoutes.push(modv.name))
+        mod.default.forEach(modv => hemoRoutes.push(modv.name))
     } else {
         routeModuleList.push(mod.default)
         hemoRoutes.push(mod.default.name)
     }
 }
 
-const routers = [Root, ...routeModuleList, admin, ...adminRouters]
+const routers = [Root, test, ...routeModuleList, admin, ...adminRouters]
 
-export { routeModuleList,hemoRoutes }
+export { routeModuleList, hemoRoutes }
 
 export default routers
