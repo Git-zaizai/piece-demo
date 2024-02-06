4fc688c91d6be2439c2c76363e5b42370d80fe12
jiangren-zaizai <2394276262@qq.com>
2024-02-06 17:30:34 +0800

    可以解析md，热更新失报错

--- src/view/Test/test/test copy.vue
+++ src/view/Test/test/test copy.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="zai-code" :style="cssVars">
-    <button class="zai-but-copy" @click="butCody"></button>
-    <span class="lang">
-      <!-- code-lang -->
-    </span>
-    <!-- code-string -->
-  </div>
+  <h1>test</h1>
+
+  <br />
+  <br />
+  <br />
+  <mdtest />
+  <br />
+  <br />
+  <br />
+  <MdLayout></MdLayout>
 </template>
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import mdtest from './tsd.md'
+import codeOutline from './CodeOutline.svg'
+import MdLayout from './md.vue'
+
 import { useCommonStore } from '@/store/index'
 const common = useCommonStore()
 const cssVars = computed(() => {
@@ -23,6 +29,18 @@ const cssVars = computed(() => {
   return vars
 })
 
+function debounce(fun, wait = 1500) {
+  let timeout = null
+  return function () {
+    if (timeout) {
+      //如果存在定时器就清空
+      clearTimeout(timeout)
+    }
+    timeout = setTimeout(() => {
+      fun.apply(this, arguments)
+    }, wait)
+  }
+}
 const codyMeg = ref('var(--vp-icon-copy)')
 let timeout
 const butCody = () => {
@@ -53,8 +71,8 @@ const butCody = () => {
 .zai-but-copy {
   direction: ltr;
   position: absolute;
-  top: 10px;
-  right: 20px;
+  top: 12px;
+  right: 24px;
   z-index: 3;
   border: 1px solid;
   border-color: transparent;
@@ -81,10 +99,45 @@ const butCody = () => {
   border-radius: 8px;
 }
 
+.view-svg-group {
+  position: absolute;
+  right: 5px;
+  top: 5px;
+  display: none;
+  align-items: center;
+}
+.view-svg {
+  width: 40px;
+  height: 40px;
+  border-radius: 4px;
+  margin-left: 5px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--vp-code-block-bg-light);
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' height='20' width='20' stroke='rgba(128,128,128,1)' stroke-width='2' viewBox='0 0 24 24'%3E%3Cpath stroke-linecap='round' stroke-linejoin='round' d='M9 5H7a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-2M9 5a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2M9 5a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2'/%3E%3C/svg%3E");
+  transition: border-color 0.2s linear, color 0.2s linear;
+  color: rgba(255, 255, 255, 0.3);
+  border: 1px solid;
+  border-color: transparent;
+  cursor: pointer;
+
+  &:hover {
+    // background-color: rgba(255, 255, 255, 0.8);
+    border-color: rgba(255, 255, 255, 0.3);
+    color: rgba(255, 255, 255, 1);
+  }
+}
+
+.svg {
+  height: 1em;
+  width: 1em;
+}
+
 .lang {
   position: absolute;
-  top: 2px;
-  right: 8px;
+  top: 6px;
+  right: 22px;
   z-index: 2;
   font-size: 12px;
   font-weight: 500;
