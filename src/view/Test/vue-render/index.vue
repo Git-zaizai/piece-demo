12961ded7dcdd70995807ab713610a2236be6d28
wqf <1186977649@qq.com>
2024-02-14 19:11:56 +0800

    最终版vite转换md 说什么都不改了

--- src/view/Test/vue-render/index.vue
+++ src/view/Test/vue-render/index.vue
@@ -12,7 +12,6 @@
       <div id="vue-render2" class="mt-20"></div>
     </div>
     <p>结论：如果只是简单的创建组件随便用，性能差不多</p>
-    <n-code :code="code" language="javascript" />
     <div>
       <div class="mt-20">
         <h4>不同组件</h4>
@@ -25,10 +24,11 @@
       <div id="vue-render-3" class="mt-20"></div>
     </div>
   </div>
+	<vueRenDerMd/>
 </template>
 <script setup lang="ts">
 import { createAppFn, hFn, createVNodeFn, createAppFn1, hFn2, createVNodeFn3 } from './order'
-import { code } from './str'
+import vueRenDerMd from './vue-render.md'
 </script>
 <style scoped lang="scss">
 .vue-order {
