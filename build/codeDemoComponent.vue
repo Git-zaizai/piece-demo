b6dbce0d800887470636b32a10a7356460874b8c
wqf <1186977649@qq.com>
2024-02-21 23:24:34 +0800

    Revert "修改了一下，novel地址"
    This reverts commit 410b6ee402d85689df28edab280a63167fa079a0.

--- build/codeDemoComponent.vue
+++ build/codeDemoComponent.vue
@@ -3,19 +3,19 @@
 		<div class="aside-zanwei"></div>
 		<div class="zai-aside">
 			<div class="content">
-				<div class="outline-marker" :style="{ top: outlineMarkerTop + 'px' }"></div>
+				<div class="outline-marker" :style="{top:outlineMarkerTop + 'px'}"></div>
 				<div class="outline-title" role="heading" aria-level="2" data-v-1588c7a1="">本页目录</div>
 				<nav>
 					<ul>
 						<li
-								v-for="(item, index) in asides"
+								v-for="(item,index) in asides"
 								:key="item.top"
-								:style="{ paddingLeft: item.type > 1 ? 1 * 15 + 'px' : '0' }"
+								:style="{paddingLeft: item.type > 1 ? 1 * 15 + 'px' : '0' }"
 						>
 							<a
 									class="outline-link"
 									:class="active === index ? 'active' : ''"
-									@click="outlineClick(item, index, $event)"
+									@click="outlineClick(item,index,$event)"
 							> {{ item.txt }} </a>
 						</li>
 					</ul>
@@ -49,20 +49,17 @@ const outlineClick = (item, index, e) => {
 }
 
 function getScrollableParent(element) {
-	let parent = element.parentNode
+	let parent = element.parentNode;
 	while (parent != null && parent !== document.body && parent !== document.documentElement) {
 		// 检查元素是否可垂直滚动
-		if (
-			parent.scrollHeight > parent.clientHeight || // 自然高度大于客户区高度
-			parent.style.overflowY === 'scroll' ||
-			parent.style.overflowY === 'auto'
-		) {
-			return parent // 如果可以，则返回该元素
+		if (parent.scrollHeight > parent.clientHeight || // 自然高度大于客户区高度
+			(parent.style.overflowY === 'scroll' || parent.style.overflowY === 'auto')) {
+			return parent; // 如果可以，则返回该元素
 		}
-		parent = parent.parentNode
+		parent = parent.parentNode;
 	}
 	// 若没有找到可以滚动的父元素，则返回null
-	return null
+	return null;
 }
 
 onMounted(() => {
@@ -155,21 +152,14 @@ const butCody = e => {
 }
 </script>
 <style lang="scss" scoped>
-ol,
-ul {
+
+ol, ul {
   list-style: none;
   margin: 0;
   padding: 0;
 }
 
-h1,
-h2,
-h3,
-h4,
-h5,
-h6,
-li,
-p {
+h1, h2, h3, h4, h5, h6, li, p {
   overflow-wrap: break-word;
 }
 
@@ -241,7 +231,7 @@ p {
   .zai-code pre {
     padding: 20px 24px;
     border-radius: 8px;
-    overflow-x: scroll;
+		overflow-x: scroll;
     scrollbar-width: none;
   }
 
@@ -276,6 +266,7 @@ p {
   overflow-y: scroll;
   scrollbar-width: none;
 
+
   .content {
     position: relative;
     border-top: 1px solid var(--vp-c-asides-divider);
@@ -297,11 +288,11 @@ p {
     border-radius: 2px;
     height: 18px;
     background-color: var(--zai-success);
-    transition: top 0.25s cubic-bezier(0, 1, 0.5, 1), background-color 0.5s, opacity 0.25s;
+    transition: top .25s cubic-bezier(0, 1, .5, 1), background-color .5s, opacity .25s;
   }
 
   .outline-title {
-    letter-spacing: 0.4px;
+    letter-spacing: .4px;
     line-height: 28px;
     font-size: 13px;
     font-weight: 600;
@@ -314,7 +305,7 @@ p {
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
-    transition: color 0.5s;
+    transition: color .5s;
     font-weight: 400;
     cursor: pointer;
     overflow-wrap: break-word;
