f1835573fd782ee5a490422da3a2e7719dd293cd
jiangren-zaizai <2394276262@qq.com>
2024-03-01 11:47:04 +0800

    添加了 Loading Demo

---
+++ src/view/Test/Loading/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="layout">
+      <div v-for="(item, index) in components" :key="index">
+        <component :is="item.default"></component>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+const components = import.meta.glob('./components/*.vue', { eager: true })
+</script>
+<style scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+}
+</style>
