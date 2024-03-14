9ebb890169548c89cbe0f15a6d3bf0a884a990a9
wqf <1186977649@qq.com>
2024-03-14 23:15:59 +0800

    路由添加sort，修改布局支持h5demo布局，

--- src/components/modal-form.vue
+++ src/components/modal-form.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 const props = defineProps<{
-  show: boolean
-  formBtnLoading?: boolean
-  displaydirective?: boolean
+	show: boolean
+	formBtnLoading?: boolean
+	displaydirective?: boolean
 }>()
 const emit = defineEmits(['update:show', 'update:formBtnLoading', 'confirmForm'])
 
@@ -22,15 +22,21 @@ const butLoading = computed({
 </script>
 
 <template>
-  <n-modal v-model:show="showModal" :display-directive="displaydirective || 'if'" :show-icon="false" preset="dialog" title="新建">
-    <slot></slot>
-    <template #action>
-      <n-space>
-        <n-button type="tertiary" @click="() => (showModal = false)">取消</n-button>
-        <n-button type="success" :loading="butLoading" @click="()=>{emit('confirmForm')}">确定</n-button>
-      </n-space>
-    </template>
-  </n-modal>
+	<n-modal
+			v-model:show="showModal"
+			:display-directive="displaydirective || 'if'"
+			:show-icon="false"
+			preset="dialog"
+			title="新建"
+	>
+		<slot></slot>
+		<template #action>
+			<n-space>
+				<n-button type="tertiary" @click="() => (showModal = false)">取消</n-button>
+				<n-button type="success" :loading="butLoading" @click="()=>{emit('confirmForm')}">确定</n-button>
+			</n-space>
+		</template>
+	</n-modal>
 </template>
 
 <style scoped lang="scss">
