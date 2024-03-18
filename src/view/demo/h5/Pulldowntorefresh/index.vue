574b2027b5d238d580a0a9c8ef0f42a4686a392b
wqf <1186977649@qq.com>
2024-03-18 21:26:02 +0800

    完整版

--- src/view/demo/h5/Pulldowntorefresh/index.vue
+++ src/view/demo/h5/Pulldowntorefresh/index.vue
@@ -1,23 +1,26 @@
 <template>
-  <div class="pull">
-    <pull-down-to-refresh :onDropdown="dropdown" :onPullupLoading="onPullupLoading">
-      <n-list hoverable clickable>
-        <n-list-item v-for="(item, index) in list" :key="index">
-          {{ item.text }}
-        </n-list-item>
-      </n-list>
-    </pull-down-to-refresh>
-    <div style="position: fixed; top: 50vh; left: 50vw">
-      <n-button @click="but">父组件 添加</n-button>
-    </div>
-    <!-- <Pulldowntorefresh2>
-      <n-list hoverable clickable>
-        <n-list-item v-for="(item, index) in list" :key="index">
-          {{ item.text }}
-        </n-list-item>
-      </n-list>
-    </Pulldowntorefresh2> -->
-  </div>
+	<div class="pull">
+		<pull-down-to-refresh
+				:on-down="dropdown"
+				:on-pull="onPullupLoading"
+		>
+			<n-list hoverable clickable>
+				<n-list-item v-for="(item, index) in list" :key="index">
+					{{ item.text }}
+				</n-list-item>
+			</n-list>
+		</pull-down-to-refresh>
+		<div style="position: fixed; top: 50vh; left: 50vw">
+			<n-button @click="but">父组件 添加</n-button>
+		</div>
+		<!-- <Pulldowntorefresh2>
+			<n-list hoverable clickable>
+				<n-list-item v-for="(item, index) in list" :key="index">
+					{{ item.text }}
+				</n-list-item>
+			</n-list>
+		</Pulldowntorefresh2> -->
+	</div>
 </template>
 <script setup lang="ts">
 import PullDownToRefresh from './Pulldowntorefresh.vue'
@@ -25,59 +28,64 @@ import Pulldowntorefresh2 from './Pulldowntorefresh2.vue'
 import { isMobile } from '@/utils'
 
 defineOptions({
-  name: 'Pulldowntorefresh'
+	name: 'Pulldowntorefresh'
 })
 
 onMounted(() => {
-  if (!isMobile()) {
-    window.$message.warning('请切换到手机端，\n web端无法操作，\n 样式也可能有问题', {
-      closable: true,
-      duration: 0,
-      showIcon: true
-    })
-  }
+	if (!isMobile()) {
+		window.$message.warning('请切换到手机端，\n web端无法操作，\n 样式也可能有问题', {
+			closable: true,
+			duration: 0,
+			showIcon: true
+		})
+	}
 })
 
 const createData = (len = 10, time = 1500) => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      let data = Array.from({ length: len }).map(() => {
-        return {
-          text: Math.random().toString(36).slice(2, 12)
-        }
-      })
-      resolve(data)
-    }, time)
-  })
+	return new Promise(resolve => {
+		setTimeout(() => {
+			let data = Array.from({ length: len }).map(() => {
+				return {
+					text: Math.random().toString(36).slice(2, 12)
+				}
+			})
+			resolve(data)
+		}, time)
+	})
 }
 const list = ref<Array<{ text: string }>>([])
 createData(50).then((data: any) => {
-  list.value = list.value.concat(data)
+	list.value = list.value.concat(data)
 })
 
-const dropdown = (fn: Function) => {
-  return createData(10, 1500).then((data: any) => {
-    list.value = list.value.concat(data)
-    fn()
-  })
+const dropdown = async (fn: Function) => {
+	return createData(10, 1500).then((data: any) => {
+		list.value = list.value.concat(data)
+		fn()
+	})
 }
-const onPullupLoading = (valueRef: any) => {
-  console.log('山旮旯')
 
-  return createData(2, 3000).then((data: any) => {
-    list.value = list.value.concat(data)
-    valueRef.value = false
-  })
+let page = 1
+const onPullupLoading = (callbcak) => {
+	return createData(2, 3000).then((data: any) => {
+		list.value = list.value.concat(data)
+		if (page > 3) {
+			callbcak('已经没有了', false)
+		} else {
+			callbcak()
+		}
+		page++
+		console.log(list.value.length)
+	})
 }
 const but = () => {
-  createData(2, 1500).then((data: any) => {
-    list.value = list.value.concat(data)
-  })
+	createData(2, 1500).then((data: any) => {
+		list.value = list.value.concat(data)
+	})
 }
 </script>
 <style scoped lang="scss">
 .pull {
   height: calc(100vh - (64px));
-  background: #ebebeb;
 }
 </style>
