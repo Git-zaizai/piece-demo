289bd06352ffa3ae617a6c2b635659b6eca220ba
wqf <1186977649@qq.com>
2024-03-18 21:26:02 +0800

    完整版

--- src/view/demo/h5/Pulldowntorefresh/Pulldowntorefresh.vue
+++ src/view/demo/h5/Pulldowntorefresh/Pulldowntorefresh.vue
@@ -1,64 +1,54 @@
 <template>
-  <div class="zai-pull" :class="{ 'hide-scroll': showScrollBar }" :style="cssVars" ref="zaiPullRef">
-    <div class="zai-pull-loaderBox">
-      <transition name="fade" mode="out-in" appear>
-        <div class="flex-alc" v-if="downRef.iconStatus < 3">
-          <n-icon
-            color="var(--success-color)"
-            size="20"
-            :component="ArrowDown"
-            class="el-transition-200"
-            :class="downRef.iconStatus === 1 ? 'rotate-0' : 'rotate-180'"
-          />
-          <span class="ml-5">{{ downRef.text }}</span>
-        </div>
-        <div class="flex-alc" v-else><i class="zai-pull-loader"></i> <span class="ml-5">刷新...</span></div>
-      </transition>
-    </div>
-    <div class="scroll-y" @scroll="pullScroll" ref="scrollYRef">
-      <slot> </slot>
-    </div>
+	<div class="zai-pull" :class="{ 'hide-scroll': showScrollBar }" :style="cssVars" ref="zaiPullRef">
+		<div class="zai-pull-loaderBox">
+			<transition name="fade" mode="out-in" appear>
+				<div class="flex-alc" v-if="downRef.iconStatus < 3">
+					<n-icon
+							color="var(--success-color)"
+							size="20"
+							:component="ArrowDown"
+							class="el-transition-200"
+							:class="downRef.iconStatus === 1 ? 'rotate-0' : 'rotate-180'"
+					/>
+					<span class="ml-5">{{ downRef.text }}</span>
+				</div>
+				<div class="flex-alc" v-else><i class="zai-pull-loader"></i> <span class="ml-5">刷新...</span></div>
+			</transition>
+		</div>
+		<div class="scroll-y" @scroll="pullScroll" ref="scrollYRef">
+			<slot></slot>
+			<p v-if="pullUpCompleted">
+				{{ pullup.text }}
+			</p>
+		</div>
 
-    <teleport to="body">
-      <div class="pull-up-loaderBox" :style="{ '--pull-up-y': pullup.y + 'px', ...cssVars }">
-        <transition name="fade" mode="out-in" appear>
-          <div class="flex-alc" v-if="pullup.iconStatus < 3">
-            <n-icon
-              color="var(--success-color)"
-              size="20"
-              :component="ArrowUpOutline"
-              class="el-transition-200"
-              :class="pullup.iconStatus === 1 ? 'rotate-0' : 'rotate--180'"
-            />
-            <span class="ml-5">{{ pullup.text }}</span>
-          </div>
-          <div class="flex-alc" v-else>
-            <i class="pull-up-loader"></i>
-            <span class="ml-5">加载...</span>
-          </div>
-        </transition>
-      </div>
-    </teleport>
-  </div>
+		<teleport to="body">
+			<div class="pull-up-loaderBox" :style="{ '--pull-up-y': pullup.y + 'px', ...cssVars }">
+				<transition name="fade" mode="out-in" appear>
+					<div class="flex-alc" v-if="pullup.iconStatus < 3">
+						<n-icon
+								color="var(--success-color)"
+								size="20"
+								:component="ArrowUpOutline"
+								class="el-transition-200"
+								:class="pullup.iconStatus === 1 ? 'rotate-0' : 'rotate--180'"
+						/>
+						<span class="ml-5">{{ pullup.text }}</span>
+					</div>
+					<div class="flex-alc" v-else>
+						<i class="pull-up-loader"></i> <span class="ml-5">加载...</span>
+					</div>
+				</transition>
+			</div>
+		</teleport>
+	</div>
 </template>
 <script setup lang="ts">
 import { useThemeVars } from 'naive-ui'
 import { ArrowUpOutline, ArrowDown } from '@vicons/ionicons5'
 
 defineOptions({
-  name: 'pull-down-to-refresh'
-})
-
-interface Props {
-  showScrollBar?: boolean
-  InfiniteDropdown?: boolean
-  onDown: () => void | Promise<void>
-  onPull: () => void | Promise<void>
-}
-
-const props = withDefaults(defineProps<Props>(), {
-  showScrollBar: false,
-  InfiniteDropdown: true
+	name: 'pull-down-to-refresh'
 })
 
 // 最大下拉距离
@@ -71,199 +61,236 @@ const PULL_UP_Y_MAX = -100
 const PULL_UP_Y_MIN = -70
 
 let startY = 0,
-  startX = 0,
-  endY = 0,
-  endX = 0,
-  distanceY = 0,
-  distanceX = 0,
-  loadLock = false,
-  viewScrollTop = 0,
-  viewScrollHeight = 0
+	startX = 0,
+	endY = 0,
+	endX = 0,
+	distanceY = 0,
+	distanceX = 0,
+	loadLock = false,
+	// 滚动距离
+	viewScrollTop = 0,
+	// 滚动高度
+	viewScrollHeight = 0
+
+interface Props {
+	showScrollBar?: boolean
+	InfiniteDropdown?: boolean
+	onDown: (fn: () => void) => void | Promise<void>
+	onPull: (fn: (pullCallback: string, pullUpCompleted: boolean) => void) => void | Promise<void>
+}
+
+const props = withDefaults(defineProps<Props>(), {
+	showScrollBar: false,
+	InfiniteDropdown: true
+})
 
 const naiveTheme = useThemeVars()
 const cssVars = computed(() => {
-  return {
-    '--success-color': naiveTheme.value.successColor,
-    '--border-color': naiveTheme.value.borderColor,
-    '--distance-y': distanceYRef.value + 'px'
-  }
+	return {
+		'--success-color': naiveTheme.value.successColor,
+		'--border-color': naiveTheme.value.borderColor,
+		'--distance-y': distanceYRef.value + 'px'
+	}
 })
 const zaiPullRef = ref<HTMLDivElement | null>(null)
 const scrollYRef = ref<HTMLDivElement | null>(null)
 const distanceYRef = ref(0)
+
 const downRef = ref({
-  iconStatus: 1,
-  text: '下拉'
+	iconStatus: 1,
+	text: '下拉'
 })
+
+const pullUpCompleted = ref(false)
 const pullup = ref({
-  y: 0,
-  iconStatus: 1,
-  text: '上拉'
+	y: 0,
+	iconStatus: 1,
+	text: '上拉'
 })
 
 function pullScroll(e: any) {
-  const { scrollHeight, clientHeight, scrollTop } = e.target
-  viewScrollTop = scrollTop + clientHeight
-  if (clientHeight + scrollTop >= scrollHeight) {
-  }
+	const { scrollHeight, clientHeight, scrollTop } = e.target
+	viewScrollTop = scrollTop + clientHeight
+	console.log('pullScroll', viewScrollTop)
 }
 
 function start(e: TouchEvent) {
-  /*	if (viewScrollTop > 0) {
+	/*	if (viewScrollTop > 0) {
 	 return
 	 }*/
-  if (loadLock) {
-    return
-  }
-  startY = e.touches[0].clientY
-  startX = e.touches[0].clientX
+	if (loadLock) {
+		return
+	}
+	startY = e.touches[0].clientY
+	startX = e.touches[0].clientX
 }
 
 function move(e: TouchEvent) {
-  /*if (viewScrollTop > 0) {
+	/*if (viewScrollTop > 0) {
 	 return
 	 }*/
-  endY = e.touches[0].clientY
-  endX = e.touches[0].clientX
-  if (loadLock) {
-    return
-  }
-  /*if (endY - startY < 0) {
+	endY = e.touches[0].clientY
+	endX = e.touches[0].clientX
+	if (loadLock) {
+		return
+	}
+	/*if (endY - startY < 0) {
 	 return
 	 }*/
 
-  distanceY = endY - startY
-  distanceX = endX - startX
+	distanceY = endY - startY
+	distanceX = endX - startX
 
-  const deg = Math.atan(Math.abs(distanceX) / distanceY) * (180 / Math.PI)
-  if (deg > DISTANCE_Y_MIN_LIMIT) {
-    ;[startY, startX] = [endY, endX]
-    return
-  }
-  let percent = (100 - distanceY * 0.5) / 100
-  percent = Math.max(0.5, percent)
+	const deg = Math.atan(Math.abs(distanceX) / distanceY) * (180 / Math.PI)
+	if (deg > DISTANCE_Y_MIN_LIMIT) {
+		;[startY, startX] = [endY, endX]
+		return
+	}
+	let percent = (100 - distanceY * 0.5) / 100
+	percent = Math.max(0.5, percent)
 
-  distanceY = distanceY * percent
+	distanceY = distanceY * percent
 
-  if (viewScrollTop === 0 && distanceY > 0) {
-    if (distanceY > DISTANCE_Y_MIN_LIMIT) {
-      downRef.value = {
-        iconStatus: 2,
-        text: '放手'
-      }
-      if (!props.InfiniteDropdown) {
-        distanceY = DISTANCE_Y_MAX_LIMIT
-      }
-    }
-    distanceYRef.value = distanceY
-  }
+	if (viewScrollTop === 0 && distanceY > 0) {
+		if (distanceY > DISTANCE_Y_MIN_LIMIT) {
+			downRef.value = {
+				iconStatus: 2,
+				text: '放手'
+			}
+			if (!props.InfiniteDropdown) {
+				distanceY = DISTANCE_Y_MAX_LIMIT
+			}
+		}
+		distanceYRef.value = distanceY
+	}
 
-  if (viewScrollTop >= viewScrollHeight && distanceY < 0) {
-    if (distanceY < PULL_UP_Y_MIN) {
-      pullup.value.iconStatus = 2
-      pullup.value.text = '放手'
-      if (!props.InfiniteDropdown) {
-        distanceY = PULL_UP_Y_MAX
-      }
-    }
-    distanceYRef.value = distanceY
-    pullup.value.y = distanceY
-  }
+	if (!pullUpCompleted.value && viewScrollTop >= viewScrollHeight && distanceY < 0) {
+		if (distanceY < PULL_UP_Y_MIN) {
+			pullup.value.iconStatus = 2
+			pullup.value.text = '放手'
+			if (!props.InfiniteDropdown) {
+				distanceY = PULL_UP_Y_MAX
+			}
+		}
+		distanceYRef.value = distanceY
+		pullup.value.y = distanceY
+	}
 }
 
 function end() {
-  /*if (viewScrollTop > 0) {
+	/*if (viewScrollTop > 0) {
 	 return
 	 }*/
-  if (loadLock) {
-    return
-  }
-  /*if (endY - startY < 0) {
+	if (loadLock) {
+		return
+	}
+	/*if (endY - startY < 0) {
 	 return
 	 }*/
 
-  if (viewScrollTop === 0 && distanceY > 0) {
-    if (distanceY < DISTANCE_Y_MIN_LIMIT) {
-      downRef.value = {
-        iconStatus: 1,
-        text: '下拉'
-      }
-      distanceYRef.value = 0
-      return
-    }
+	if (viewScrollTop === 0 && distanceY > 0) {
+		if (distanceY < DISTANCE_Y_MIN_LIMIT) {
+			downRef.value = {
+				iconStatus: 1,
+				text: '下拉'
+			}
+			distanceYRef.value = 0
+			return
+		}
 
-    distanceYRef.value = DISTANCE_Y_MIN_LIMIT
-    downRef.value.iconStatus = 3
-    loadLock = true
+		distanceYRef.value = DISTANCE_Y_MIN_LIMIT
+		downRef.value.iconStatus = 3
+		loadLock = true
 
-    setTimeout(() => {
-      loadLock = false
-      distanceY = 0
-      distanceYRef.value = 0
+		if (props.onDown) {
+			props.onDown(downCallback)
+		} else {
+			setTimeout(() => {
+				downCallback()
+			}, 2000)
+		}
+	}
 
-      downRef.value = {
-        iconStatus: 1,
-        text: '下拉'
-      }
-    }, 1200)
-  }
-  if (viewScrollTop >= viewScrollHeight && distanceY < 0) {
-    if (distanceY > PULL_UP_Y_MIN) {
-      pullup.value = {
-        y: 0,
-        iconStatus: 1,
-        text: '上拉'
-      }
-      distanceYRef.value = 0
-      return
-    }
 
-    loadLock = true
-    distanceYRef.value = PULL_UP_Y_MIN
-    pullup.value.iconStatus = 3
-    pullup.value.y = PULL_UP_Y_MIN
-    setTimeout(() => {
-      loadLock = false
-      distanceY = 0
-      distanceYRef.value = 0
-      pullup.value = {
-        y: 0,
-        iconStatus: 1,
-        text: '上拉'
-      }
-    }, 2000)
-  }
+	if (viewScrollTop >= viewScrollHeight && distanceY < 0) {
+		if (distanceY > PULL_UP_Y_MIN) {
+			pullup.value = {
+				y: 0,
+				iconStatus: 1,
+				text: '上拉'
+			}
+			distanceYRef.value = 0
+			return
+		}
+
+		loadLock = true
+		distanceYRef.value = PULL_UP_Y_MIN
+		pullup.value.iconStatus = 3
+		pullup.value.y = PULL_UP_Y_MIN
+
+		if (props.onPull) {
+			props.onPull(pullCallback)
+		} else {
+			setTimeout(() => {
+				pullCallback()
+			}, 2000)
+		}
+	}
+}
+
+function downCallback() {
+	loadLock = false
+	distanceY = 0
+	distanceYRef.value = 0
+
+	downRef.value = {
+		iconStatus: 1,
+		text: '下拉'
+	}
+}
+
+function pullCallback(pullText = '上拉', completed?: boolean) {
+	loadLock = false
+	distanceY = 0
+	distanceYRef.value = 0
+	pullup.value = {
+		y: 0,
+		iconStatus: 1,
+		text: pullText
+	}
+	if (typeof completed !== 'undefined') {
+		pullUpCompleted.value = completed
+	}
 }
 
 // 初始化MutationObserver来监听插槽内容的变化
 const observer = new MutationObserver(() => {
-  nextTick(() => {
-    const pullDom = zaiPullRef.value
-    viewScrollHeight = pullDom.scrollHeight + pullDom.clientHeight
-  })
+	nextTick(() => {
+		viewScrollHeight = scrollYRef.value.scrollHeight
+	})
 })
 
 onMounted(() => {
-  const pullDom = zaiPullRef.value
-  if (pullDom) {
-    pullDom.addEventListener('touchstart', start, { passive: false })
-    pullDom.addEventListener('touchmove', move, { passive: false })
-    pullDom.addEventListener('touchend', end, { passive: false })
-    viewScrollHeight = pullDom.scrollHeight + pullDom.clientHeight
-    observer.observe(scrollYRef.value as HTMLDivElement, {
-      childList: true, // 观察子节点的变化
-      subtree: true // 观察整个子树
-    })
-  }
+	const pullDom = zaiPullRef.value
+	if (pullDom) {
+		pullDom.addEventListener('touchstart', start, { passive: false })
+		pullDom.addEventListener('touchmove', move, { passive: false })
+		pullDom.addEventListener('touchend', end, { passive: false })
+		observer.observe(scrollYRef.value as HTMLDivElement, {
+			childList: true, // 观察子节点的变化
+			subtree: true // 观察整个子树
+		})
+	}
 })
 
 onUnmounted(() => {
-  const pullDom = zaiPullRef.value
-  if (pullDom) {
-    pullDom.removeEventListener('touchstart', start)
-    pullDom.removeEventListener('touchmove', move)
-    pullDom.removeEventListener('touchend', end)
-  }
+	const pullDom = zaiPullRef.value
+	if (pullDom) {
+		pullDom.removeEventListener('touchstart', start)
+		pullDom.removeEventListener('touchmove', move)
+		pullDom.removeEventListener('touchend', end)
+		observer.disconnect()
+	}
 })
 </script>
 <style scoped lang="scss">
