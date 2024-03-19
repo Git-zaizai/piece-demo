b9d42eedfc08860fb69bb74e5ef7c74735341941
jiangren-zaizai <2394276262@qq.com>
2024-03-19 12:02:40 +0800

    合并 Dev 完毕

--- build/createLoader.ts
+++ build/createLoader.ts
@@ -3,70 +3,44 @@ import { getHighlighter, bundledLanguages } from 'shiki'
 import { createVueComponent, getCodeView } from './getCodeComponnent'
 
 export default async () => {
-	const getVueCode = await createVueComponent()
-	// 创建高亮渲染器
-	const highlighter = await getHighlighter({
-		themes: ['one-dark-pro'],
-		// @ts-ignore
-		langs: Object.keys(bundledLanguages)
-	})
-	const md = markdown()
+  const getVueCode = await createVueComponent()
+  // 创建高亮渲染器
+  const highlighter = await getHighlighter({
+    themes: ['one-dark-pro'],
+    // @ts-ignore
+    langs: Object.keys(bundledLanguages)
+  })
+  const md = markdown()
 
-	// 保存原有的规矩函数 暂时用不上
-	const code_block = md.renderer.rules.code_block
-	const fence = md.renderer.rules.fence
+  // 保存原有的规矩函数 暂时用不上
+  const code_block = md.renderer.rules.code_block
+  const fence = md.renderer.rules.fence
 
-	// 重写函数
-	// @ts-ignore
-	function codeRules(tokens: any, idx: number, options: any, env: any, slf: any): string {
-		const lang = tokens[idx].info
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-		console.log('lang',lang)
-		if (!transformers) {
-			transformers = {
-				name: "@shikijs/markdown-it:block-class",
-				code(node) {
-					node.properties.class = `language-${ options.lang }`;
-				}
-			}
-		}
->>>>>>> 9a62177 (vite解析md最终版)
-=======
->>>>>>> 1b9889d (最终版vite转换md 说什么都不改了)
+  // 重写函数
+  // @ts-ignore
+  function codeRules(tokens: any, idx: number, options: any, env: any, slf: any): string {
+    const lang = tokens[idx].info
 
-		const code_html = highlighter.codeToHtml(tokens[idx].content,
-			{
-				lang,
-				theme: 'one-dark-pro',
-				transformers: [
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1b9889d (最终版vite转换md 说什么都不改了)
-					{
-						name: "@shikijs/markdown-it:block-class",
-						code(node) {
-							node.properties.class = `language-${ lang }`;
-						}
-					}
-<<<<<<< HEAD
-=======
-					transformers
->>>>>>> 9a62177 (vite解析md最终版)
-=======
->>>>>>> 1b9889d (最终版vite转换md 说什么都不改了)
-				]
-			})
+    const code_html = highlighter.codeToHtml(tokens[idx].content, {
+      lang,
+      theme: 'one-dark-pro',
+      transformers: [
+        {
+          name: '@shikijs/markdown-it:block-class',
+          code(node) {
+            node.properties.class = `language-${lang}`
+          }
+        }
+      ]
+    })
 
-		return getCodeView(lang, code_html)
-	}
+    return getCodeView(lang, code_html)
+  }
 
-	md.renderer.rules.fence = codeRules
-	md.renderer.rules.code_block = codeRules
+  md.renderer.rules.fence = codeRules
+  md.renderer.rules.code_block = codeRules
 
-	return (str: string) => {
-		return getVueCode(md.render(str))
-	}
+  return (str: string) => {
+    return getVueCode(md.render(str))
+  }
 }
