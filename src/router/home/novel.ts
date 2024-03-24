4f486f37a83bd5ae3d33c811289ded85ae1a9fea
wqf <1186977649@qq.com>
2024-03-25 00:02:14 +0800

    修改hemo信息

--- src/router/home/novel.ts
+++ src/router/home/novel.ts
@@ -4,15 +4,36 @@ const novel: AppRouteRecordRaw = {
   path: '/novel',
   name: 'novel',
   meta: {
-    title: '小说'
+    title: '其他'
   },
   children: [
     {
-      path: 'http://116.204.106.221/gadgets/n',
-      name: 'http://116.204.106.221/gadgets/n',
+      path: 'https://docs.xiaoheizi.one/',
+      name: 'https://docs.xiaoheizi.one/',
       meta: {
+        title: '崽崽前端囧途',
         navigation: {
-          contentText: '记录一下看过的小说，网址还有评价'
+          contentText: '切图崽的囧途'
+        }
+      }
+    },
+    {
+      path: 'https://novel.xiaoheizishiba.top/',
+      name: 'https://novel.xiaoheizishiba.top/',
+      meta: {
+        title: '崽崽记录站 v3',
+        navigation: {
+          contentText: '崽崽记录站 v3 vite+react18+hox'
+        }
+      }
+    },
+    {
+      path: 'http://116.204.106.221/gadgets/online-html/novel.html',
+      name: 'http://116.204.106.221/gadgets/online-html/novel.html',
+      meta: {
+        title: '崽崽记录站 v2',
+        navigation: {
+          contentText: '崽崽记录站 v2 vue3+html构建'
         }
       }
     },
@@ -20,8 +41,9 @@ const novel: AppRouteRecordRaw = {
       path: 'http://175.178.151.160/n',
       name: 'http://175.178.151.160/n',
       meta: {
+        title: '崽崽记录站 v1',
         navigation: {
-          contentText: '记录一下看过的小说，网址还有评价'
+          contentText: '崽崽记录站 v1 Jquery构建'
         }
       }
     }
