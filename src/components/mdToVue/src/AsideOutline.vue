bd6af09ffc77496e40d3201487f3bcad9a0f0492
jiangren-zaizai <2394276262@qq.com>
2024-04-13 16:09:44 +0800

    基本扣成功，目前是组件状态

--- src/components/mdToVue/src/AsideOutline.vue
+++ src/components/mdToVue/src/AsideOutline.vue
@@ -2,30 +2,43 @@
 import { ref, shallowRef, onMounted } from 'vue'
 import { MenuItem } from './outline'
 import OutlineItem from './OutlineItem.vue'
+import { useThemeVars } from 'naive-ui'
 
-defineProps<{
-  headers: MenuItem[]
+const props = defineProps<{
+  headers: MenuItem[],
+  top: number
 }>()
+
+const markerStyle = computed(() => {
+  if (typeof props.top === 'number') {
+    return {
+      top: `${props.top}px`,
+      opacity: props.top > 0 ? 1 : 0
+    }
+  }
+  return { top: `0px`, opacity: 0 }
+})
 
 const container = ref()
 const marker = ref()
 const route = useRoute()
+const cssVars = computed(() => {
+  return {
+    '--n-success-color': useThemeVars().value.successColor
+  }
+})
 </script>
 
 <template>
-  <div
-    class="VPDocAsideOutline"
-    :class="{ 'has-outline': headers && headers.length > 0 }"
-    ref="container"
-    role="navigation"
-  >
+  <div class="VPDocAsideOutline" :class="{ 'has-outline': Array.isArray(headers) && headers.length > 0 }"
+    :style="cssVars" ref="container" role="navigation">
     <div class="content">
-      <div class="outline-marker" ref="marker" />
+      <div class="outline-marker" ref="marker" :style="markerStyle" />
 
-      <div class="outline-title" role="heading" aria-level="2">{{ route.name }}</div>
+      <div class="outline-title" role="heading" aria-level="2">{{ route.meta.title }}</div>
 
       <nav aria-labelledby="doc-outline-aria-label">
-        <span class="visually-hidden" id="doc-outline-aria-label"> Table of Contents for current page </span>
+        <!-- <span class="visually-hidden" id="doc-outline-aria-label"> Table of Contents for current page </span> -->
         <OutlineItem :headers="headers" :root="true" />
       </nav>
     </div>
@@ -58,7 +71,7 @@ const route = useRoute()
   width: 2px;
   border-radius: 2px;
   height: 18px;
-  background-color: var(--vp-c-brand-1);
+  background-color: var(--n-success-color);
   transition: top 0.25s cubic-bezier(0, 1, 0.5, 1), background-color 0.5s, opacity 0.25s;
 }
 
